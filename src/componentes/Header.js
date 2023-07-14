9aaaf1896a79cd6535c1f96e8a2f370f189b9603
nhoelya <noeliafalkiewicz@gmail.com>
2023-07-14 16:06:27 -0300

    termine la primera pantalla

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,16 +1,32 @@
 import './Header.css'
+import logo from './logo.png'
 
 function Header () {
     return (
         
-        <header class="alert alert-danger" role="alert">
-            
-            <h2>LOGO</h2>
+        <header class="alert" role="alert">
+            <div className='logoContenedor shadow-lg p-3 mb-5 bg-body-tertiary rounded rounded-circle'>
+               <img src={logo} className='logo' ></img> 
+            </div>
             <nav>
-                <a href=''>Home</a>
-                <a href=''>About</a>
-                <a href=''>Works</a>
-                <a href=''>Contact</a>
+                <ul class="nav justify-content-end">
+                     <li class="nav-item">
+                       <a class="link-danger link-offset-2 link-offset-3-hover link-underline
+                       link-underline-opacity-0 link-underline-opacity-50-hover link-opacity-50-hover" href="#">Inicio</a>
+                     </li>
+                    <li class="nav-item">
+                       <a class="link-danger link-offset-2 link-offset-3-hover link-underline
+                       link-underline-opacity-0 link-underline-opacity-50-hover link-opacity-50-hover" href="#">Acerca de</a>
+                    </li>
+                    <li class="nav-item">
+                       <a class="link-danger link-offset-2 link-offset-3-hover link-underline
+                       link-underline-opacity-0 link-underline-opacity-50-hover link-opacity-50-hover" href="#">Trabajos</a>
+                    </li>
+                    <li class="nav-item">
+                       <a class="link-danger link-offset-2 link-offset-3-hover link-underline
+                       link-underline-opacity-0 link-underline-opacity-50-hover link-opacity-50-hover" href='#'>Contacto</a>
+                    </li>
+               </ul>
             </nav>
 
         </header> 
--- src/componentes/Principal.js
+++ src/componentes/Principal.js
@@ -1,13 +1,13 @@
 import Header from './Header'
 import './Principal.css'
-import mancha from './mancha-rosada-entera.jpg'
+import mancha from './foto1-modificada.png'
  
 function Principal () {
 
     return(
-        <div>
-            
-         <section class="alert alert-primary" role="alert">
+        
+        <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+            <section class="border border-white alert alert-primary shadow-lg p-3 mb-5 bg-body-tertiary rounded" role="alert">
               <Header/>
               <div className='contenedorPrincipal'>
                 <div className='contenedorSecundario'>
@@ -16,14 +16,16 @@ function Principal () {
                      Soy Nhoelya
                     </h1>
                     <h3>Desarrolladora Web</h3>
-                    <button class="btn btn-warning">Hire me</button>
+                    <button class="btn btn-warning">Contratar</button>
                 </div>
                 <img src={mancha} className='manchaGrande' alt="mancha"></img>   
               </div>
               
-         </section>
-        
+            </section>
         </div>
+        
+        
+        
         
        
         
