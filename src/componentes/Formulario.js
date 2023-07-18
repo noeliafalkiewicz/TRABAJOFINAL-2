504e5a7ec42ade5f8e76c56655244037233e582f
nhoelya <noeliafalkiewicz@gmail.com>
2023-07-17 23:43:54 -0300

    cambios para activar formulario

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -22,7 +22,7 @@ function Formulario (){
     }
 
     function enviarDatos(event){
-        event.preventDefault()    
+            
     }
 
 
@@ -31,7 +31,7 @@ return (
     <>
         <div className='row my-5 justify-content-md-center'>
             <div className='formulario shadow-sm p-1 mb-3 bg-body-tertiary rounded'>
-                <form action='https://formsubmit.co/noeliafalkiewicz@email.com" method="POST' className='contenidoformulario'>
+                <form action="https://formsubmit.co/noeliafalkiewicz@gmail.com" method="POST" className='contenidoformulario'>
                     <div className='form-group'>
                         <label htmlFor="nombre"></label>
                         <input className='input border border-primary-subtle rounded-3' type='text' placeholder='Nombre/Compañia' value={nombre} name='nombre' onChange={actualizaNombre} ></input>
@@ -44,9 +44,12 @@ return (
                         <label htmlFor="nombre"></label>
                         <textarea className='input texto border border-primary-subtle rounded-3' type='text' placeholder='Deja tu mensaje...' value={mensaje} name='mensaje' onChange={actualizaMensaje}></textarea>
                    </div>
+                   <input type="hidden" name="_autoresponse" value="your custom message"></input>
+                   
+
                    <img className='mancharosaformulario' src={mancharosaformulario}></img>
 
-                   <button onClick={enviarDatos} class="boton btn btn-warning"><strong>Enviar mensaje</strong></button>
+                   <button type='submit' onClick={enviarDatos} class="boton btn btn-warning"><strong>Enviar mensaje</strong></button>
 
                    <p className='comentario'>Te respondere dentro de las 24 hs</p>
                  
